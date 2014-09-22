002427e332cf8322ffffbae47fbafa26c34a763e
Caleb Jones <bionicle.dragon@gmail.com>
2014-09-21 23:46:08 -0400

    Resolve #2 - Add deck creation
    - Insert deck into localStorage
    Change indents in markup
    Add #deck-list to markup
    Add reset button for debugging

--- js/main.js
+++ js/main.js
@@ -6,30 +6,72 @@ $(function() {
             + "(you're probably using Internet Explorer). "
             + "Please upgrade to a modern browser.");
     }
-    cardsets = []
-    cardset = {
-        card_template: null,
-        cards: null
-    }
+
+    $("#reset").click(function() {
+        if (confirm("Reset all the local storage?")) {
+            localStorage.clear();
+        }
+    });
 
     function submitForm() {
-        name = $("form").children(".name").val();
+        var name = $("form").children(".name").val();
         if (name === "undefined" || !name) {
             // Fail!
             console.log("That name is invalid!");
             return;
         }
-        card = {
+
+        var card_template = {
             name: name,
             attrs: []
         }
         $("form").children('.item').each(function() {
-            attribute = {
+            var attribute = {
                 name: $(this).children('[type=text]').val(),
                 identifies: $(this).children('[type=checkbox]:checked').length > 0
             };
-            card.attrs.push(attribute);
+            card_template.attrs.push(attribute);
         });
-        console.log(JSON.stringify(card));
+
+        if (localStorage[name]) {
+            console.log("That already exists!");
+        } else {
+            addDeck(newDeck(card_template));
+        }
+    }
+
+    function newDeck(template) {
+        var deck = {
+            name: template.name,
+            card_template: template,
+            cards: [],
+            progress: []
+        }
+        return deck;
+    }
+
+    function getDecks() {
+        var decks = localStorage["decks"]
+        if (decks === "undefined" || !decks) {
+            decks = []
+        } else {
+            decks = JSON.parse(localStorage["decks"]);
+        }
+        return decks;
+    }
+
+    function addDeck(deck) {
+        localStorage[deck.name] = JSON.stringify(deck);
+        var decks = getDecks();
+        decks.push(deck.name);
+        localStorage["decks"] = JSON.stringify(decks);
+        updateDecks();
+    }
+
+    function updateDecks() {
+        var decks = getDecks();
+        for (var i = 0, l = decks.length; i < l; i++) {
+            decks[i];
+        }
     }
 });
